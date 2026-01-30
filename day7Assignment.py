4253036afad8b1d85f0817b3e617cdc2469924ae
Naresh-Bohara <nareshbohara0533@gmail.com>
2026-01-30 09:39:10 +0545

    day-7 assignment completed.

--- day7Assignment.py
+++ day7Assignment.py
@@ -10,7 +10,7 @@ odd_numbers = [i for i in range(1, 21) if i%2 != 0]
 print("List of odd numberes form 1 to 20: ", odd_numbers)
 
 #q3. create a list of square of numbers from 1-20
-square_numbers = [i*i for i in range(1, 21)]
+square_numbers = [i**2 for i in range(1, 21)]
 print("square numbers from 1 to 20: ", square_numbers)
 
 #q4. create a list of lenght of each words in ["apple","orange","kiwi"]
@@ -19,7 +19,7 @@ words_length = [len(word) for word in words]
 print("length of words: ", words_length)
 
 #q5. create a list of squares of only even numbers from 1-20
-even_numbers_square = [i*i for i in range(1, 21) if i%2 == 0]
+even_numbers_square = [i**2 for i in range(1, 21) if i%2 == 0]
 print("Even numbers square from 1 to 20: ", even_numbers_square)
 
 #q6. create a list of numbers divisible by 3 from 1-100
