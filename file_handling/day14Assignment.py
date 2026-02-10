5160b630c88f36548dd1f187b23105ab2038269e
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-02-10 09:19:05 +0545

    day-14 assignment completed

--- file_handling/day14Assignment.py
+++ file_handling/day14Assignment.py
@@ -1,12 +1,14 @@
-with open('file_handling/hello.txt', 'r') as file:
+file_path = "file_handling/hello.txt"
+with open(file_path, 'r') as file:
     data = file.read()
     print(data)
 
 target = input("Enter the word to remove from the file: ").strip()
-if target.lower() in data.lower():
+if target in data:
     print(f"'{target}' found in the file.")
     data = data.replace(target, "")
-    with open('file_handling/hello.txt', 'w') as file:
+    with open(file_path, 'w') as file:
         file.write(data)
+    print(f"'{target}' has been removed from the file.")
 else:
     print(f"'{target}' not found in the file.")
