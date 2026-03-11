3a8453a697a2901fdfcf475a4c22fa582eb2fb47
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-03-11 20:13:46 +0545

    code updated upto Passing Data from View to Template in Django

--- django/myapp/views.py
+++ django/myapp/views.py
@@ -2,7 +2,35 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
-
+    data = [
+        {
+        'name': 'Naresh',
+        'age': 25,
+        'city': 'Dhangadhi',
+        'marks': 58
+    },
+    {
+        'name': 'Ravi',
+        'age': 30,
+        'city': 'Kathmandu',
+        'marks': 90
+    },
+    {
+        'name': 'Suresh',
+        'age': 28,
+        'city': 'Pokhara',
+        'marks': 61
+    },
+    {
+        'name': 'Mahesh',
+        'age': 32,
+        'city': 'Biratnagar',
+        'marks': 85
+    }
+    ]
+    context = {'data': data}
+    return render(request, 'index.html', context)
 def home(request):
-    return render(request, 'home.html')
+    students = ["Naresh", "Ravi", "Suresh", "Mahesh"]
+    context = {'students': students}
+    return render(request, 'home.html', context)
