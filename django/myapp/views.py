4cd3949b4ca6808940f3ed4a6ffa3ae828af6dd7
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-03-16 08:03:04 +0545

    code updated upto orm basics

--- django/myapp/views.py
+++ django/myapp/views.py
@@ -4,12 +4,42 @@ from .models import Students
 # Create your views here.
 
 def index(request):
-    students = Students.objects.all()
+    students = Students.objects.get(id=1)
     context = {'students': students}
     return render(request, 'index.html', context)
 
-
 def home(request):
-    students = ["Naresh", "Ravi", "Suresh", "Mahesh"]
+    # students = Students.objects.all()
+    students = Students.objects.filter(age__gt=17)
+
+    # // student object
+#     students = Students.objects.create(
+#     name='John Doe',
+#     age=20,
+#     city='New York',
+#     marks=85,
+#     email='john.doe@example.com'
+# )
+
+#     for student update object
+#     student = Students.objects.get(id=1)
+#     student.name = 'Jane Doe'
+#     student.save()
+
+#      for student delete object
+#      student = Students.objects.get(id=1)
+#      student.delete()
+
+#     for ordering
+#     students = Students.objects.order_by('name')
+
+#     for ordering in reverse
+#     students = Students.objects.order_by('-name')
+
+#     for ordering by multiple fields
+#     students = Students.objects.order_by('age', 'name')
+
+
+
     context = {'students': students}
     return render(request, 'home.html', context)
